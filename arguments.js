9dcc1a8df4137633c951d1133ee09150c2120d5b
nicktamburro <nicholastamburro@gmail.com>
2018-09-21 10:37:12 -0700

    more fun with args and ...rest

--- arguments.js
+++ arguments.js
@@ -3,6 +3,36 @@ function whatever(a, b, c){
         console.log("yo");
     }
     console.log(arguments);
+    console.log(arguments.length);
+    //arguments is NOT an object, you can's use array methods on it
 }
 
 whatever(1,2,3,4,5);
+
+//here's another args thing
+//THIS IS COOL because you don't have to know how many numbers you're going to sum
+//it just takes however many you give it!
+function sum(){
+    var sum = 0;
+    for (var i=0; i<arguments.length; i++){
+        sum += arguments[i];
+    }
+    console.log("sum: " + sum);
+}
+
+sum(1,2,3,4,5);
+
+//rewritten with rest parameters!
+function sum(a, ...otherNumbers){
+   let sum = a;
+   for(let i=0; i<otherNumbers.length; i++){
+       sum += otherNumbers[i];
+   }
+  console.log("rest sum: " + sum);
+    }
+  
+
+
+
+
+//here's another one
