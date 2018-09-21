790e14351c4473b72667eeac6c713df191e9305b
nicktamburro <nicholastamburro@gmail.com>
2018-09-20 21:34:50 -0700

    adding jasmine tests

--- argsParams.js
+++ argsParams.js
@@ -10,3 +10,9 @@ var rule = (daimyo) => performAction(daimyo, "ruling");
 
 skulk("Hattori");
 rule("Oda Nobunaga");
+
+describe("skulk", function() {
+  it("tells us that a given ninja is skulking", function() {
+    expect(skulk("Hattori")).toBe("Hattori - skulking");
+  });
+});
