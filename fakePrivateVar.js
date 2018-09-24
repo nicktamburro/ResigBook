60cc2ba5d54f1dfd1188867b15e35e32fa63b210
nicktamburro <nicholastamburro@gmail.com>
2018-09-24 13:58:04 -0700

    approximating private vars (like in java):

---
+++ fakePrivateVar.js
@@ -0,0 +1,27 @@
+//a fake plastic var-i-a-ble...
+function Ninja(){
+    //remember CAPS!
+    var feints = 0;
+    //ninja has these two methods, that's why this... 
+    //because when we make a new instance, we want those to have the same methods
+    this.getFeints = function(){
+        return feints;
+    };
+    this.feint = function(){
+        feints++;
+    };
+}
+
+var ninja1 = new Ninja();
+ninja1.feint();
+
+if(ninja1.feints === undefined){
+    console.log("we can't see these from out here!");
+}if(ninja1.getFeints() === 1){
+    console.log("nowwww we can see it! it's 1!");
+}
+
+var ninja2 = new Ninja();
+if(ninja2.getFeints() === 0){
+    console.log("and we can see that this is zero");
+}
