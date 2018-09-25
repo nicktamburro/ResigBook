aec55a3134d5fd454fba2fae79d198a1d3e4fe30
nicktamburro <nicholastamburro@gmail.com>
2018-09-25 13:38:13 -0700

    using bind()

---
+++ bind.js
@@ -0,0 +1,28 @@
+<html>
+<head>
+    </head>
+    <body>
+<button id="test">Click!</button>
+
+{/*<script>
+    var button = {
+        clicked: false,
+        click: function(){
+            this.clicked = true;
+            if(button.clicked){
+                console.log("button has been clicked!");
+            }
+        }
+    };
+
+    var elem = document.getElementById("test");
+    elem.addEventListener("click", button.click.bind(button));
+
+    var boundFunction = button.click.bind(button);
+    if(boundFuction != button.click){
+        console.log("we made a new function!");
+    }
+
+</script>*/}
+</body>
+</html>
