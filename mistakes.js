2e198d1ea9fa1a6b90bba5c079b2ef07c3f7d9c4
nicktamburro <nicholastamburro@gmail.com>
2018-09-29 21:51:39 -0700

    fixed the mistake issue

--- mistakes.js
+++ mistakes.js
@@ -13,17 +13,20 @@ for(let i = 0; i<sortedArray.length; i++){
 if(sortedArray[i] === sortedArray[i+1]){
     console.log("double")
     mistakeCount += 1;
+    continue;
 }
 // out of range
 if(sortedArray[i] < range[0] || sortedArray[i] > range[1]){
     console.log("out of range");
     mistakeCount += 1;
+    continue;
 }
 
 // one is missing
 if(sortedArray[i+1] != undefined && sortedArray[i+1] - sortedArray[i] != 1){
     console.log("one missing");
     mistakeCount += (sortedArray[i+1] - sortedArray[i] - 1);
+    continue;
 }
 
 
