2205534e3dd4ea379070645a74163715099411df
nicktamburro <nicholastamburro@gmail.com>
2018-11-09 11:48:01 -0800

    reviewing default params

--- defaultParams.js
+++ defaultParams.js
@@ -1,5 +1,7 @@
 //this is the old pre-ES6 way
 function performAction(ninja, action){
+    //if the action is undefined, make it "skulking" 
+    //if it IS defined, make it whatever it already is
     action = typeof action === "undefined" ? "skulking" : action;
     return ninja + " " + action;
 }
@@ -11,6 +13,8 @@ performActon("Yagyu", "sneaking") //will say "Yagya sneaking"
 //====================================================================================
 //heres the default parameters way
 //you can just put it in the arguments!
+//
+//here, the action will be skulking, UNLESS we say otherwise
 function performAction(ninja, action = "skulking"){
     return ninja + " " + action;
 }
