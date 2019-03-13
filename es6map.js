1d65ccb6203b4e051756bfebb3b5da3aa4f8d2de
nicktamburro <nicholastamburro@gmail.com>
2019-03-12 23:02:40 -0700

    es6 map

--- es6map.js
+++ es6map.js
@@ -1,4 +1,14 @@
+//here we have an array
 const continents = ['Savage Land', 'Wakanda', 'Latvertia', 'Danger Room'];
+
+//we make a variable called message
+//which takes continents and applies map to it
+//
+//a function called c, I think just because "continents" 
+//which prints "Hello" and continents[0], and join make space
+//then "Hello" and continents[1], and a space, etc etc
 const message = continents.map(c => `Hello ${c}!`).join(" ");
 
+//and then we make a component, which is a paragraph tag
+//which gets filled with message
 const component = <p>{message}</p>;
