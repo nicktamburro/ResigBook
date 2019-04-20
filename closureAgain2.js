9dbb32792a987deb635e704f1c3b4f9a378a43c0
nicktamburro <nicholastamburro@gmail.com>
2019-04-19 21:34:20 -0700

    closures

--- closureAgain2.js
+++ closureAgain2.js
@@ -1,10 +1,16 @@
 let outerValue = "samurai";
+//okay outer value, straight up
 let later;
+//later is what we'll use later!
+
 
 function outerFunction(){
+    //2. ...which creates the inner value
     let innerValue = "ninja";
 
+    //and gives us this inner function
     function innerFunction(){
+        //these ifs are both true...
         if(outerValue === "samurai"){
             console.log("I can see the samurai");
         }
@@ -14,9 +20,12 @@ function outerFunction(){
 
 
     }
+    //while we're here, we set a value to later
             later = innerFunction;
 }
 
+//1. first we call the outerFunction
 outerFunction();
-
+//the scope of the function doesn't exist, but 
+//we can still access it
 later();
